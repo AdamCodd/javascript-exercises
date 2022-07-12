fab8a22cf867ba74b1afe036236862b683222122
Adam <49641859+AdamCodd@users.noreply.github.com>
2022-07-13 01:42:07 +0200

    Add other solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,16 @@
 const reverseString = function (word) {
-    //return word.split('').reverse().join('');
     let string = '';
     for (let i = word.length - 1; i >= 0; i--) {
         string += word[i];
     }
     return string;
 };
+
+/* Other solution */
+/*
+const reverseString = function (word) {
+    return word.split('').reverse().join('');
+}
 
 // Do not edit below this line
 module.exports = reverseString;
