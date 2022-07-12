864eeaf26e60b9d57b9998c994d8a02b13e6da47
Adam <49641859+AdamCodd@users.noreply.github.com>
2022-07-12 22:03:56 +0200

    Second exercise done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, repeat) {
+    if (repeat > 0) {
+        return string.padEnd(string.length * repeat, string);
+    }
+    else if (repeat === 0 || string === '') {
+        return '';
+    }
+    else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
