c9451b160c08a2218282dcdc3211fd3232d7a877
Adam <49641859+AdamCodd@users.noreply.github.com>
2022-07-14 01:33:17 +0200

    Solution exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase().match(/[a-z]+/g).join('');
+    let reversed = string.split('').reverse().join('');
+    if (string === reversed) return true;
+    return false;
 };
+
+/*** Other solution *****/
+/*
+
+const palindromes = function(string) {
+  processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+  return (
+    processedString
+      .split("")
+      .reverse()
+      .join("") == processedString
+  );
+};
+
+*/
 
 // Do not edit below this line
 module.exports = palindromes;
