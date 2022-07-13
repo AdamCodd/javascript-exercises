92ce36efe9f60c9f975b09470476e49832fd8c4c
Adam <49641859+AdamCodd@users.noreply.github.com>
2022-07-13 16:11:31 +0200

    Solution exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    if (year % 4 === 0 && (year % 100 !== 0 || year % 100 === 0 && year % 400 === 0)) {
+        return true;
+    }
+    return false;
 };
+
+/* Other solution */
+/* 
+const leapYears = function(year) {
+  return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0);
+};
+*/
 
 // Do not edit below this line
 module.exports = leapYears;
