3c067a9a0aeda79848fd120aa52ae619429235e3
Adam <49641859+AdamCodd@users.noreply.github.com>
2022-07-13 16:41:20 +0200

    Solution exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,23 @@
-const ftoc = function() {
-
+const ftoc = function (farenheit) {
+  let celcius = 0;
+  return celcius = Math.round((farenheit - 32) * (5 / 9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (celcius) {
+  let farenheit = 0;
+  return farenheit = Math.round((celcius * (9 / 5) + 32) * 10) / 10;
 };
+
+/* Solution */
+/*
+const ftoc = function(f) {
+ return Math.round((f - 32) * (5/9) * 10) / 10;
+};
+
+const ctof = function(c) {
+  return Math.round(((c * 9/5) + 32) * 10) / 10;
+};
+*/
 
 // Do not edit below this line
 module.exports = {
