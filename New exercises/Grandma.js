0b7d7977518e65ea727f018783e407506bdef4b2
Adam <49641859+AdamCodd@users.noreply.github.com>
2022-07-14 17:11:27 +0200

    Add 3 "BYE" to quit

--- New exercises/Grandma.js
+++ New exercises/Grandma.js
@@ -1,11 +1,22 @@
+// Improved Grandma, the program stops when the user input "BYE" three times.
+
 let input;
+let num = 0;
 while (true) {
     input = prompt('Talk to your grandma!');
-    if (input === "BYE") break;
     if (input === input.toUpperCase()) {
         alert('NO, NOT SINCE 19' + Math.floor(Math.random() * 100));
+        if (input === "BYE") {
+            num++;
+            if (num === 3) {
+                break;
+            }
+        } else {
+            num = 0;
+        }
     }
     else {
         alert('HUH?! SPEAK UP, SONNY!');
+        num = 0;
     }
 }
