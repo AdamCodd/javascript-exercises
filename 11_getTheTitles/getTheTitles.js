27a6fe435c7b83cc0e8045b47a77c06b24b9ff6e
Adam <49641859+AdamCodd@users.noreply.github.com>
2022-07-14 15:21:49 +0200

    Solution exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,20 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    titles = [];
+    books.forEach(book => {
+        titles.push(book.title);
+    });
+    return titles;
 };
+
+/***** Other solution *********/
+/*
+
+const getTheTitles = function(array) {
+  return array.map(book => book.title);
+};
+
+module.exports = getTheTitles;
+*/
 
 // Do not edit below this line
 module.exports = getTheTitles;
