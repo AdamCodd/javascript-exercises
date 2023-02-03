4442c019e4ee3fabb23a7e46087ee95d205566e6
Adam <49641859+AdamCodd@users.noreply.github.com>
2023-02-03 18:28:42 +0100

    Solution to pigLatin

--- pigLatin/pigLatin.js
+++ pigLatin/pigLatin.js
@@ -1,6 +1,27 @@
 function pigLatin(string) {
-
+    let arr = string.split(" ");
+    function matchElem(elem) {
+        if (elem[0].match(/[aeiou]/)) {
+            return (elem + "ay");
+        }
+        else {
+            let i = 0;
+            let cons = "";
+            while (!elem[i].match(/[aeiou]/)) {
+                cons += elem[i];
+                // Add the "u" with the "q" and skip it in the next iteration
+                if (elem[i].match(/q/) && elem[i + 1].match(/u/)) {
+                    cons += 'u';
+                    i = i + 1;
+                }
+                i++;
+            }
+            return (elem.slice(i) + cons + "ay");
+        }
+    }
+    let result = arr.map(elem => matchElem(elem));
+    return result.join(" ");
 };
-  
+
 // Do not edit below this line
 module.exports = pigLatin;
